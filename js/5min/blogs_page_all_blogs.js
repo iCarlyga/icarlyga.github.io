a62156d0295c8a0c6540b2bc1dfd0cfec2f4e82c
iCarlyga <58372053+iCarlyga@users.noreply.github.com>
2019-12-02 00:04:23 -0600

    Create blogs_page_all_blogs.js

---
+++ js/5min/blogs_page_all_blogs.js
@@ -0,0 +1,15 @@
+var ALL_BLOGS = {
+Freddie: {
+  2019: {
+    1: {
+      519: {
+       title: 'iExist',
+       teaser: 'The iBlog is alive!',
+       date: 1270861200,
+       url: '/iBlogs/entry1.html'}
+    }
+    }
+   }
+
+}
+};
