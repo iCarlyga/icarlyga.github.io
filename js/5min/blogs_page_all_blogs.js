ccefd42611494bedd583994d2b122bf746376aa3
iCarly.ga <58372053+iCarlyga@users.noreply.github.com>
2019-12-07 15:18:33 -0600

    Update blogs_page_all_blogs.js

--- js/5min/blogs_page_all_blogs.js
+++ js/5min/blogs_page_all_blogs.js
@@ -1,12 +1,24 @@
 var ALL_BLOGS = {
 Freddie: {
   2019: {
-    0: {
+    11: {
       1: {
        title: 'iExist',
        teaser: 'The iBlog is alive!',
-       date: 1270861200,
+       date: 1261524600,
        url: '/iBlogs/entry1.html'}
+    }
+   }
+
+},
+Sam: {
+  2019: {
+    11: {
+      2: {
+       title: 'Bear!',
+       teaser: 'Cool Bear Story!',
+       date: 1261524600,
+       url: '/iBlogs/entry2.html'}
     }
     }
    }
