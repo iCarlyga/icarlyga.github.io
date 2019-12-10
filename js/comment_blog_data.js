e4700bf92446adef5b1fc242306c72f205f97b51
iCarly.ga <58372053+iCarlyga@users.noreply.github.com>
2019-12-10 14:48:13 -0600

    Update comment_blog_data.js

--- js/comment_blog_data.js
+++ js/comment_blog_data.js
@@ -1,8 +1,10 @@
 var ALL_BLOG_COMMENTS = {
   "1": [
-    {"member": "Test", "comment": "this is a comment!"}
+    {"member": "Test", "comment": "this is a comment!"},
+    {"member": "Test", "comment": "bro... cum pee"}
   ],
   "2": [
+    {"member": "Test", "comment": "bro... cum pee"},
     {"member": "Test", "comment": "bro... cum pee"}
   ]
 };
