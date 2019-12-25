ac845d729582f1d1a9162554abdd9f6e5eabf635
iCarly.ga <58372053+iCarlyga@users.noreply.github.com>
2019-12-24 23:48:25 -0600

    Update blogs_page_all_blogs.js

--- js/5min/blogs_page_all_blogs.js
+++ js/5min/blogs_page_all_blogs.js
@@ -1,4 +1,28 @@
 var ALL_BLOGS = {
+Carly: {
+  2019: {
+    11: {
+      3: {
+       title: 'Merry Christmas',
+       teaser: 'Since you guys have been such awesome fans, we decided to get you a little something this year!',
+       date: 1261524600,
+       url: '/iBlogs/entry3.html'}
+    }
+    }
+   },
+
+Sam: {
+  2019: {
+    11: {
+      2: {
+       title: 'Bear!',
+       teaser: 'Cool Bear Story!',
+       date: 1261524600,
+       url: '/iBlogs/entry2.html'}
+    }
+    }
+
+   },
 Freddie: {
   2019: {
     11: {
@@ -11,16 +35,5 @@ Freddie: {
    }
 
 },
-Sam: {
-  2019: {
-    11: {
-      2: {
-       title: 'Bear!',
-       teaser: 'Cool Bear Story!',
-       date: 1261524600,
-       url: '/iBlogs/entry2.html'}
-    }
-    }
-   }
 
 };
