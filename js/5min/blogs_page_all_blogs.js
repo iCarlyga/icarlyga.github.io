206a5aa2e5b3f371224eb970c427c6783496f8dc
iCarly.ga <58372053+iCarlyga@users.noreply.github.com>
2020-01-18 13:51:11 -0600

    Add files via upload

--- js/5min/blogs_page_all_blogs.js
+++ js/5min/blogs_page_all_blogs.js
@@ -9,8 +9,17 @@ Carly: {
        url: '/iBlogs/entry3.html'}
     }
     }
+,
+  2020: {
+    0: {
+      5: {
+       title: 'MY BIGGEST PET PEEVES',
+       teaser: 'Read my list of all-time WORST PET PEEVES!',
+       date: 1233338400,
+       url: '/iBlogs/entry5.html'}
+    }
+    }
    },
-
 Sam: {
   2019: {
     11: {
@@ -20,8 +29,17 @@ Sam: {
        date: 1261524600,
        url: '/iBlogs/entry2.html'}
     }
+   }
+,
+  2020: {
+    0: {
+      5: {
+       title: 'Things I\'ve Changed in the School\'s Computer System',
+       teaser: 'Read what I changed in the school\'s computer system!',
+       date: 1233338400,
+       url: '/iBlogs/entry5.html'}
     }
-
+    }
    },
 Freddie: {
   2019: {
@@ -33,5 +51,16 @@ Freddie: {
        url: '/iBlogs/entry1.html'}
     }
    }
+,
+  2020: {
+    0: {
+      4: {
+       title: 'Carly\'s Birthday Gift',
+       teaser: 'I need your help with Carly\'s Birthday Gift! Click here...',
+       date: 1200153600,
+       url: '/iBlogs/entry4.html'}
+    }
+   }
+
 }
 };
